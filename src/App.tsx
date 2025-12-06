c228d72b6b8eb5abd707922c8b7faacfe44e2e67
bm78g <isntglutencakejustbread@gmail.com>
2025-12-05 23:56:30 -0600

    feat: add distinction between work and break time, add option to change them in settings, fixed progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,23 @@ import OptionsButton from './components/OptionsButton'
 import TimerDisplay from './components/TimerDisplay'
 
 export default function App() {
-  const [startTime, setStartTime] = useState(1500)
+  const [workTime, setWorkTime] = useState(1500)
+  const [breakTime, setBreakTime] = useState(300)
+  const [working, setWorking] = useState(true)
 
-  const [seconds, setSeconds] = useState(startTime)
+  const [seconds, setSeconds] = useState(workTime)
   const [running, setRunning] = useState(false)
 
   const runningRef = useRef(running)
   useEffect(() => { runningRef.current = running }, [running])
+  const workingRef = useRef(working)
+  useEffect(() => { workingRef.current = working }, [working])
+  const workTimeRef = useRef(workTime)
+  useEffect(() => { workTimeRef.current = workTime }, [workTime])
+  const breakTimeRef = useRef(breakTime)
+  useEffect(() => { breakTimeRef.current = breakTime }, [breakTime])
+  const secondsRef = useRef(seconds)
+  useEffect(() => { secondsRef.current = seconds }, [seconds])
 
   const [showOptions, setShowOptions] = useState(false)
 
@@ -23,7 +33,15 @@ export default function App() {
       setSeconds(prev => {
         if (prev > 0)
           return prev - 1
-        clearInterval(intervalId)
+
+        if (workingRef.current){
+          setWorking(false)
+          setSeconds(breakTimeRef.current)
+        } else {
+          setWorking(true)
+          setSeconds(workTimeRef.current)
+        }
+        progress = workingRef.current ? workTimeRef.current : breakTimeRef.current
         return 0
       })
     }, 1000)
@@ -34,24 +52,34 @@ export default function App() {
   let minutes = Math.floor(seconds / 60) % 60
   let hours = Math.floor(seconds / 3600)
   let secondsFormat = seconds % 60
-
-  let progress = startTime - seconds
+  
+  let maxTime = working ? workTime : breakTime
+  let progress = maxTime - seconds
 
   // Commented temporarily because the control buttons are removed.
-  // const modifyTime = (diff: number) => {
-  //   setSeconds(prev => {
-  //     if (prev + diff < 0)
-  //       return 0
-  //     return prev + diff
-  //   })
-  //   setStartTime(prev => {
-  //     if (prev + diff < 0)
-  //       return 0
-  //     return prev + diff
-  //   })
-  //   console.log(seconds, startTime)
-  //   progress = startTime - seconds
-  // }
+  const modifyWorkTime = (diff: number) => {
+    const newWorkTime = Math.max(0, workTime + diff)
+    setWorkTime(newWorkTime)
+    if (workingRef.current)
+      setSeconds(newWorkTime)
+    progress = newWorkTime - secondsRef.current
+  }
+
+  const modifyBreakTime = (diff: number) => {
+    const newBreakTime = Math.max(0, breakTime + diff)
+    setBreakTime(newBreakTime)
+    if (!workingRef.current)
+      setSeconds(newBreakTime)
+    progress = newBreakTime - secondsRef.current
+  }
+
+  const resetTime = () => {
+    if (workingRef.current)
+      setSeconds(workTime)
+    else
+      setSeconds(breakTime)
+    setRunning(false)
+  }
 
   const toggleOptions = (enable: boolean) => {
     setShowOptions(enable)
@@ -61,7 +89,21 @@ export default function App() {
     <>
       {showOptions && (
         <div className='options-block' onClick={() => toggleOptions(false)}>
-          <div className="options-panel" onClick={(e) => e.stopPropagation()}></div>
+          <div className="options-panel" onClick={(e) => e.stopPropagation()}>
+            <h1>Work Time</h1>
+            <h1>{Math.floor(workTime / 60).toString().padStart(2, "0")}
+              : {(workTime % 60).toString().padStart(2, "0")}
+            </h1>
+            <button onClick={() => modifyWorkTime(60)}>+1 min</button>
+            <button onClick={() => modifyWorkTime(-60)}>-1 min</button>
+
+            <h1>Break Time</h1>
+            <h1>{Math.floor(breakTime / 60).toString().padStart(2, "0")}
+              : {(breakTime % 60).toString().padStart(2, "0")}
+            </h1>
+            <button onClick={() => modifyBreakTime(60)}>+1 min</button>
+            <button onClick={() => modifyBreakTime(-60)}>-1 min</button>
+          </div>
         </div>
       )}
       <div className='header-block'>
@@ -76,12 +118,13 @@ export default function App() {
           <div className='control-btn-container'>
             <button className='control-btn' onClick={() => setRunning(true)}>Start</button>
             <button className='control-btn' onClick={() => setRunning(false)}>Stop</button>
-            <button className='control-btn' onClick={() => {setSeconds(startTime); setRunning(false)}}>Reset</button>
+            <button className='control-btn' onClick={() => resetTime()}>Reset</button>
+            <button onClick={() => setSeconds(1)}>Set sec to 1</button>
           </div>
           <input
             type="range"
             min="0"
-            max={startTime}
+            max={maxTime}
             value={progress}
             readOnly
             style={{ width: "300px" }}/>
