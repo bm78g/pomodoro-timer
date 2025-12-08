7bde883dd4d383c2d8eb6ba05e5927724e65dbe9
bm78g <73971640+bm78g@users.noreply.github.com>
2025-12-08 01:34:08 -0600

    style: restructure options panel for visual aesthetics

--- src/components/OptionsPanel/OptionsPanel.tsx
+++ src/components/OptionsPanel/OptionsPanel.tsx
@@ -12,19 +12,29 @@ interface OptionsPanelProps {
 export default function OptionsPanel({workTime, breakTime, modifyWorkTime,modifyBreakTime}: OptionsPanelProps) {
     return (
         <div className="options-panel" onClick={(e) => e.stopPropagation()}>
-            <h1>Work Time</h1>
-            <h1>{Math.floor(workTime / 60).toString().padStart(2, "0")}
-              : {(workTime % 60).toString().padStart(2, "0")}
-            </h1>
-            <button onClick={() => modifyWorkTime(60)}>+1 min</button>
-            <button onClick={() => modifyWorkTime(-60)}>-1 min</button>
+          <h1>Options</h1>
+          <hr></hr>
+          <h1 className='options-timer-header'>Set Timer</h1>
+          <div className='options-timer'>
+            <div className='options-timer-worktime'>
+              <p>Work Time</p>
+              <h1 className='options-timer-display'>{Math.floor(workTime / 60).toString().padStart(2, "0")}
+                : {(workTime % 60).toString().padStart(2, "0")}
+              </h1>
+              <button onClick={() => modifyWorkTime(60)}>+1 min</button>
+              <button onClick={() => modifyWorkTime(-60)}>-1 min</button>
+            </div>
 
-            <h1>Break Time</h1>
-            <h1>{Math.floor(breakTime / 60).toString().padStart(2, "0")}
-              : {(breakTime % 60).toString().padStart(2, "0")}
-            </h1>
-            <button onClick={() => modifyBreakTime(60)}>+1 min</button>
-            <button onClick={() => modifyBreakTime(-60)}>-1 min</button>
+            <div className='options-timer-breaktime'>
+              <p>Break Time</p>
+              <h1 className='options-timer-display'>{Math.floor(breakTime / 60).toString().padStart(2, "0")}
+                : {(breakTime % 60).toString().padStart(2, "0")}
+              </h1>
+              <button onClick={() => modifyBreakTime(60)}>+1 min</button>
+              <button onClick={() => modifyBreakTime(-60)}>-1 min</button>
+            </div>
+          </div>
+          <hr></hr>
         </div>
     )
 }
